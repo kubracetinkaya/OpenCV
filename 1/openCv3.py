a962a58a0152972985c16f552e4bcd5de80a8e40
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-07-06 23:43:25 +0300

    Update openCv3.py

--- 1/openCv3.py
+++ 1/openCv3.py
@@ -2,5 +2,5 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=cv2.imread('saat.jpg',0)
-cv2.imwrite('saat.png',img)
+img=cv2.imread('WALL-E.jpg',0)
+cv2.imwrite('WALL-E.png',img)
