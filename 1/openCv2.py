14c1bbe6ca7442c9d90416a2f7fb8493ca4a499c
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-07-06 23:43:02 +0300

    Update openCv2.py

--- 1/openCv2.py
+++ 1/openCv2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img=cv2.imread('saat.jpg', cv2.IMREAD_GRAYSCALE)
+img=cv2.imread('WALL-E.jpg', cv2.IMREAD_GRAYSCALE)
 plt.imshow(img, cmap='gray', interpolation='bicubic')
 plt.plot([50,100],[80,500],'c',linewidth=5)
 plt.show()
