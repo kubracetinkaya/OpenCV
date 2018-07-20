aa11ad3bae28c15c6874e0669aadc3ec0516f131
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-07-20 16:33:33 +0300

    Update color_histograms.py

--- 5/color_histograms.py
+++ 5/color_histograms.py
@@ -20,6 +20,31 @@ for (chan,color) in zip(chans,colors):
     hist=cv2.calcHist([chan],[0],None,[256],[0,256])
     plt.plot(hist,color=color)
     plt.xlim([0,256])
-    
+ 
+
+fig=plt.figure()
+ax=fig.add_subplot(131)
+hist=cv2.calcHist([chans[1],chans[0]],[0,1],None,[32,32],[0,256,0,256])
+p=ax.imshow(hist,interpolation='nearest')
+ax.set_title('2D color histogram for G and B')
+plt.colorbar(p)
+
+ax=fig.add_subplot(132)
+hist=cv2.calcHist([chans[1],chans[2]],[0,1],None,[32,32],[0,256,0,256])
+p=ax.imshow(hist,interpolation='nearest')
+ax.set_title('2D color histogram for G and R')
+plt.colorbar(p)
+
+ax=fig.add_subplot(133)
+hist=cv2.calcHist([chans[0],chans[2]],[0,1],None,[32,32],[0,256,0,256])
+p=ax.imshow(hist,interpolation='nearest')
+ax.set_title('2D color histogram for B and R')
+plt.colorbar(p)
+
+print("2D histogram shape: %s, with %d values" % (hist.shape, hist.flatten().shape[0]))
+
+hist=cv2.calcHist([image],[0,1,2],None,[8,8,8],[0,256,0,256,0,256])
+print("3D histogram shape: %s, with %d values" % (hist.shape, hist.flatten().shape[0]))
+
 plt.show()
 cv2.waitKey(0)
