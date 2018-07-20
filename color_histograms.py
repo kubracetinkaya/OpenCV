52589def54d5a2af55488fcd4296eafdfaf08944
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-07-20 15:48:47 +0300

    Add files via upload

---
+++ color_histograms.py
@@ -0,0 +1,25 @@
+import cv2
+import argparse
+import numpy as np
+from matplotlib import pyplot as plt
+
+ap=argparse.ArgumentParser()
+ap.add_argument('-i','--image',required=True,help='Path to the image')
+args=vars(ap.parse_args())
+image=cv2.imread(args['image'])
+cv2.imshow('Original',image)
+
+chans=cv2.split(image)
+colors=('b','g','r')
+plt.figure()
+plt.title('Flattened color histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+
+for (chan,color) in zip(chans,colors):
+    hist=cv2.calcHist([chan],[0],None,[256],[0,256])
+    plt.plot(hist,color=color)
+    plt.xlim([0,256])
+    
+plt.show()
+cv2.waitKey(0)
